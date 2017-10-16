5a35af19b5c2a0465988614766388ba14e48fa54
hani2284 <hani2284@colorado.edu>
2017-10-15 21:36:45 -0600

    Working intro paragraph w/ gifs

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,58 @@
+$('a').hover(
+       function(){ $(this).removeClass('hide') },
+       function(){ $(this).addClass('hide') }
+)
 
+$(".lasp-link").hover(
+               function() {
+                   $( ".wrapper" ).addClass( "lasp" );
+               }, function() {
+                   $( ".wrapper" ).removeClass( "lasp" );
+               }
+           );
+
+$(".sneaker-link").hover(
+              function() {
+                  $( ".wrapper" ).addClass( "sneaker" );
+              }, function() {
+                  $( ".wrapper" ).removeClass( "sneaker" );
+              }
+          );
+
+$(".userex-link").hover(
+              function() {
+                    $( ".wrapper" ).addClass( "ux" );
+                }, function() {
+                    $( ".wrapper" ).removeClass( "ux" );
+                }
+            );
+
+$(".fashion-link").hover(
+              function() {
+                    $( ".wrapper" ).addClass( "fashion" );
+                }, function() {
+                    $( ".wrapper" ).removeClass( "fashion" );
+                }
+            );
+
+$(".min-link").hover(
+              function() {
+                    $( ".wrapper" ).addClass( "minimal" );
+                }, function() {
+                    $( ".wrapper" ).removeClass( "minimal" );
+                }
+            );
+
+$(".design-link").hover(
+              function() {
+                    $( ".wrapper" ).addClass( "design" );
+                }, function() {
+                    $( ".wrapper" ).removeClass( "design" );
+                }
+            );
+$('design-link').hide();
+$('write-link').hide();
+$('min-link').hide();
+$('userex-link').hide();
+$('fashion-link').hide();
+$('sneaker-link').hide();
